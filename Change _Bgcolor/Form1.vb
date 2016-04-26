37ced1a8e9ac7e7fcd26a5cc25b7985f0194b7ab
jubinmathew1995 <jubinmathew1995@gmail.com>
2016-04-26 21:50:12 +0530

    Initial commit

---
+++ Change _Bgcolor/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    Dim r, g, b As Single
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+      
+
+        If IsNumeric(TextBox1.Text) And IsNumeric(TextBox2.Text) And IsNumeric(TextBox3.Text) Then
+            r = TextBox1.Text
+            g = TextBox2.Text
+            b = TextBox3.Text
+
+            Me.BackColor = Color.FromArgb(r Mod 255, g Mod 255, b Mod 255)
+        Else
+
+
+
+            MsgBox("Invalid input",MsgBoxStyle.Critical)
+
+
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ README.txt
@@ -0,0 +1,6 @@
+BGCOLOR 
+
+It is a Windows form application which sets the background color in whatever RGB code provided by the user. 
+RGB values varies in the range of 0 to 255. Entering values other than the specified values raises a error 
+and doesnot set the specified colour to the background.
+
